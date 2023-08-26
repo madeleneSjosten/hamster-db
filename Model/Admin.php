0754e74a3942767deec32c9e507f687cf0fdcd5f
Madelene Sjosten <madelenesjosten@gmail.com>
2023-08-26 10:30:22 +0200

    construct

--- Model/Admin.php
+++ Model/Admin.php
@@ -1,5 +1,16 @@
 <?php
 
 class Admin {
-    
+
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+
+    function __construct(int $id, string $firstName, string $lastName, string $email){
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+    }    
 }
